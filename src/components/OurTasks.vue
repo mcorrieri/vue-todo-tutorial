aa020af8ef2d81da0d5478bbe91ea2a5cbdd7adc
Michael <Mlcorrieri@gmail.com>
2022-04-12 14:46:42 -0400

    working on create task

---
+++ src/components/OurTasks.vue
@@ -0,0 +1,26 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <SingleTask
+      @toggle-reminder="$emit('toggle-reminder', task.id)"
+      @delete-task="$emit('delete-task', task.id)"
+      :task="task"
+    />
+  </div>
+</template>
+
+<script>
+import SingleTask from "./SingleTask.vue";
+
+export default {
+  name: "OurTasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    SingleTask,
+  },
+  emits: ["delete-task", "toggle-reminder"],
+};
+</script>
+
+<style></style>
---
+++ src/components/TheButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="handleClick()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "TheButton",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    handleClick() {
+      console.log("hey hotdog man");
+    },
+  },
+};
+</script>
+
+<style></style>
